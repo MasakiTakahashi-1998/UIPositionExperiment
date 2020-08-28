90ddd7b7f7d418d3276a9ea218d7a311c260cbcc
takahashi.masaki <takahashi.masaki@techcross.co.jp>
2020-08-28 16:27:47 +0900

    Unityプロジェクト  - 解像度対応

---
+++ Assets/Coffee/FixResolution.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class FixResolution : MonoBehaviour
+{
+    [SerializeField] private RectTransform thisRectTransform = default;
+    [SerializeField] private RectTransform parentRectTransform;
+    
+    private void Reset()
+    {
+        SetReferenceResolution();
+    }
+    
+    private void Awake()
+    {
+        SetReferenceResolution();
+        UpdateRectTransformScale();
+    }
+    
+#if UNITY_EDITOR || !UNITY_2019_3_OR_NEWER
+    void Update()
+    {
+        UpdateRectTransformScale();
+    }
+#endif
+    
+    /// <summary>
+    /// 参照するCanvasScalerとRectTransformを指定する
+    /// また、ScreenMatchModeをExpendに指定する
+    /// </summary>
+    private void SetReferenceResolution()
+    {
+        thisRectTransform = GetComponent<RectTransform>();
+        parentRectTransform = transform.parent.GetComponent<RectTransform>();
+        CanvasScaler canvasScaler = transform.parent.GetComponent<CanvasScaler>();
+        if (canvasScaler != null)
+        {
+            canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+        }
+    }
+    
+    /// <summary>
+    /// アスペクト比とセーフエリアに合わせてスケールを変更する
+    /// UnityEditorでは、iPhoneXRの解像度を指定した場合、該当端末のセーフエリアを再現する
+    /// </summary>
+    private void UpdateRectTransformScale()
+    {
+        var sizeDelta = parentRectTransform.sizeDelta;
+        thisRectTransform.offsetMin = new Vector2(Screen.safeArea.x * (sizeDelta.x / Screen.width), Screen.safeArea.y * (sizeDelta.y/ Screen.height));
+        thisRectTransform.offsetMax = new Vector2(-Screen.safeArea.x * (sizeDelta.x / Screen.width), 0);
+        thisRectTransform.anchoredPosition = new Vector2(0.5f,1);
+    }
+}
---
+++ Assets/Coffee/FixResolution_RenderTexture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class FixResolution_RenderTexture : MonoBehaviour
+{
+    Vector2 nowResolution = Vector2.zero;
+
+    [SerializeField] public Canvas mainCanvas;
+    [SerializeField] public RawImage rawImage;
+    [SerializeField] public Camera targetCamera;
+    [SerializeField] private GraphicsFormat _graphics = GraphicsFormat.None;
+    [SerializeField] private DepthBuffer _depthBuffer = DepthBuffer.Twentyfour;
+    
+    private GraphicsFormat nowgraphics = GraphicsFormat.None;
+    private DepthBuffer nowdepthBuffer = DepthBuffer.None;
+
+    void Update()
+    {
+        if (nowResolution != mainCanvas.GetComponent<RectTransform>().sizeDelta || nowgraphics != _graphics || nowdepthBuffer != _depthBuffer)
+        {
+            nowgraphics = _graphics;
+            nowdepthBuffer = _depthBuffer;
+            nowResolution = mainCanvas.GetComponent<RectTransform>().sizeDelta;
+            
+            if (targetCamera.targetTexture != null)
+            {
+                targetCamera.targetTexture.Release();
+            }
+
+            targetCamera.targetTexture = new RenderTexture((int) nowResolution.x, (int)nowResolution.y, (int)nowdepthBuffer, nowgraphics)
+            {
+                name = $"{(int) nowResolution.x}x{(int) nowResolution.y}"
+            };
+            rawImage.texture = targetCamera.targetTexture;
+        }
+    }
+
+    enum DepthBuffer
+    {
+        None = 0,
+        Sixteen = 16,
+        Twentyfour = 24,
+    }
+}
