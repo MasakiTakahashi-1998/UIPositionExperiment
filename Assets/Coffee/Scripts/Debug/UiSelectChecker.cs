7c5dcced00d5b665dae56acd78f178aa551a9f6f
Markey <hw16a111@oecu.jp>
2020-09-23 13:42:37 +0900

    no message

---
+++ Assets/Coffee/Scripts/Debug/UiSelectChecker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiSelectChecker : MonoBehaviour
+{
+    public void DoClick()
+    {
+        Debug.Log("DoClick");
+    }
+}
