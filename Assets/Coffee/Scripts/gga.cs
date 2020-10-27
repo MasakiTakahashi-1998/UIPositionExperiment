e7170672f488eaa465ba425a0c16fed54bbed877
takahashi.masaki <takahashi.masaki@techcross.co.jp>
2020-10-27 16:28:01 +0900

    チュートリアル基盤(入力制限)の作成 セーフエリア対応の修正 Camareのdepth管理

---
+++ Assets/Coffee/Scripts/gga.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class gga : MonoBehaviour
+{
+    public List<Camera> cameras;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetCameraClearFlags();
+    }
+    
+    private void SetCameraClearFlags()
+    {
+        cameras = FindObjectsOfType<Camera>().Where(x => x.targetTexture == null).ToList();
+        var maxDepth = cameras.Min(x => x.depth);
+
+        foreach (var value in cameras)
+        {
+            if (Math.Abs(maxDepth - value.depth) > 0)
+            {
+                value.clearFlags = CameraClearFlags.Depth;
+                continue;
+            }
+            value.clearFlags = CameraClearFlags.SolidColor;
+            value.backgroundColor = Color.white;
+        }
+    }
+}
---
+++ Assets/Plugins/FixResolution/Editor/FixResolutionEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Plugin.FixResolution.Editor
+{
+    public class FixResolutionEditor : EditorWindow
+    {
+        static string prefabPath = "Assets/Plugins/FixResolution/Prefab/";
+        
+        /// <summary>
+        /// 右クリックから解像度対応用のキャンバスを作成するためのエディタ拡張
+        /// </summary>
+        [MenuItem("GameObject/UI/FixResolution/Canvas")]
+        public static void Create()
+        {
+            //アセットをロード
+            GameObject prefabCanvas = AssetDatabase.LoadAssetAtPath<GameObject>($"{prefabPath}FixCanvas.prefab");
+            GameObject prefabCamera = AssetDatabase.LoadAssetAtPath<GameObject>($"{prefabPath}CanvasCamera.prefab");
+            // GameObject prefabEventSystem = AssetDatabase.LoadAssetAtPath<GameObject>($"{prefabPath}EventSystem.prefab");
+
+            //ロードしたアセットを配置する
+            GameObject canvas = PrefabUtility.InstantiatePrefab(prefabCanvas) as GameObject;
+            GameObject camera = PrefabUtility.InstantiatePrefab(prefabCamera) as GameObject;
+            // GameObject eventSystem = PrefabUtility.InstantiatePrefab(prefabEventSystem) as GameObject;
+            canvas.GetComponent<Canvas>().worldCamera = camera.GetComponent<Camera>();
+            
+            //Undo対応
+            Undo.RegisterCreatedObjectUndo(canvas, "Create New GameObject");
+            Undo.RegisterCreatedObjectUndo(camera, "Create New GameObject");
+            // Undo.RegisterCreatedObjectUndo(eventSystem, "Create New GameObject");
+            
+            //Prefabのリンクを解除する
+            PrefabUtility.UnpackPrefabInstance(canvas,PrefabUnpackMode.Completely,InteractionMode.AutomatedAction);
+            PrefabUtility.UnpackPrefabInstance(camera,PrefabUnpackMode.Completely,InteractionMode.AutomatedAction);
+            // PrefabUtility.UnpackPrefabInstance(eventSystem,PrefabUnpackMode.Completely,InteractionMode.AutomatedAction);
+        }
+    }
+}
